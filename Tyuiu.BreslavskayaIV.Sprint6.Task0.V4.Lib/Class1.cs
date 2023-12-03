3ff6326a57e8a947253543fa7a8613126079f6e9
breslavskayairrra@gmail.com <breslavskayairrra@gmail.com>
2023-12-03 20:55:44 +0300

    Add project files.

---
+++ Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Lib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Lib
+{
+    public class DataService: ISprint6Task0V4
+    {
+        public double Calculate(int x)
+        {
+            double res = 2.4 * x * x * x + 0.4 * x * x - 1.4 * x + 4.1;
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Test/Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Test.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+using Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Lib;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tyuiu.BreslavskayaIV.Sprint6.Task0.V4.Test
+{
+	[TestClass]
+	public class DataServiceTest
+	{
+		[TestMethod]
+		public void ValidCalc() {
+			DataService ds = new DataService();
+			double res = ds.Calculate(3);
+			double wait = 68.3;
+			Assert.AreEqual(wait, res);
+		}
+	};
+}
